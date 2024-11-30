1810b729128c1b22ecece3b4355bc7c17535f94d
uubulb <uub@kuzu.uk>
2024-11-30 13:10:12 +0800

    complete agent script (posix)

--- agent/install.sh
+++ agent/install.sh
@@ -150,7 +150,9 @@ install() {
         path=$(printf "%s" "$NZ_AGENT_PATH/config-$random.yml")
     fi
 
-    _cmd="sudo $NZ_AGENT_PATH/nezha-agent service -c "$path" install"
+    env="NZ_SERVER=$NZ_SERVER NZ_CLIENT_SECRET=$NZ_CLIENT_SECRET NZ_TLS=$NZ_TLS NZ_DISABLE_AUTO_UPDATE=$NZ_DISABLE_AUTO_UPDATE NZ_DISABLE_FORCE_UPDATE=$DISABLE_FORCE_UPDATE NZ_DISABLE_COMMAND_EXECUTE=$NZ_DISABLE_COMMAND_EXECUTE NZ_SKIP_CONNECTION_COUNT=$NZ_SKIP_CONNECTION_COUNT"
+
+    _cmd="sudo $env $NZ_AGENT_PATH/nezha-agent service -c "$path" install"
     if ! eval "$_cmd"; then
         err "Install nezha-agent service failed"
         exit 1
